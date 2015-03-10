3a8cdcdae365f59164e5c0ba613f3f89876d89f8
Joe Levering <coyoteejl@gmail.com>
2015-03-09 21:51:02 -0500

    Add readme

--- init.js
+++ init.js
@@ -2,16 +2,22 @@ angular.module("spaceRace", [])
   .controller("gameController", ['$interval', function gameController($interval){
     var gc = this;
 
+    var Enemy = function() {
+      this.health = 10;
+      this.name = "Enemy"
+    };
+
     gc.bulletCount = 0;
     gc.soldierCount = 0;
     gc.resources = 0;
-    gc.currentTargetHealth = 10;
+    gc.currentTarget = new Enemy();
 
     gc.villagesDestroyed = 0;
 
+
     gc.shootBullets = function(numberOfBullets) {
       gc.bulletCount += numberOfBullets;
-      gc.currentTargetHealth -= numberOfBullets;
+      gc.currentTarget.health -= numberOfBullets;
       handleAndCheckDeath();
     }
 
@@ -20,10 +26,19 @@ angular.module("spaceRace", [])
       gc.resources -= 10;
     }
 
+    gc.showSoldierButton = function() {
+      return (gc.soldierCount > 0 || gc.resources >= 10);
+    }
+
+    gc.disableSoldierButton = function() {
+      return (gc.resources < 10);
+    }
+
     function handleAndCheckDeath() {
-      if (gc.currentTargetHealth <= 0) {
-        overkillAmount = gc.currentTargetHealth * -1
-        gc.currentTargetHealth = 10 - overkillAmount;
+      if (gc.currentTarget.health <= 0) {
+        overkillAmount = gc.currentTarget.health * -1
+        gc.currentTarget = new Enemy();
+        gc.currentTarget.health -= overkillAmount;
         gc.resources += 10;
         gc.villagesDestroyed += 1;
         handleAndCheckDeath(); // In case overkillAmount is >= new targetHealth
---
+++ readme.md
@@ -0,0 +1,33 @@
+Conquer increasingly difficult foes on your path to universal domination!
+
+#####Village era:
+
+1 enemy has 10 health and provides 1 resource  
+1 hut has 5 enemies and provides 10 resources  
+1 village has 5 huts and provides 100 resources  
+1 tribe has 10 villages and provides 2000 resources
+
+#####Empire era:
+
+1 city has (5 tribe equivalent) and provides 20,000 resources  
+1 castle has 10 cities and provides 400,000 resources  
+1 fiefdom has 10 castles and provides 8,000,000 resources  
+1 region has 10 fiefdoms and provides 160,000,000 resources  
+
+#####Modern era:
+
+1 state has 10 regions and provides 3,200,000,000 resources  
+1 country has 10 states  
+1 alliance has 10 countries  
+1 continent has 10 alliances  
+
+#####Space era:
+
+1 planet has 10 continents  
+1 planetary system has 10 planets  
+1 galaxy has 10 planetary systems  
+1 cluster has 10 galaxies  
+1 supercluster has 10 clusters  
+1 universe has 10 superclusters  
+1 galaxy filament has 100 universes  
+1 large quasar group has 10 galaxy filaments  
