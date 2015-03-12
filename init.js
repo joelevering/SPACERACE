995b46865bf741804be2b5b2b598293842977b5f
Joe Levering <coyoteejl@gmail.com>
2015-03-11 21:23:00 -0500

    Refactor bullet-shooting logic to eliminate recursiveness

--- init.js
+++ init.js
@@ -3,10 +3,20 @@ angular.module("spaceRace", ['ngCookies'])
     var gc = this;
     var versionNumber = 1
 
-    var Enemy = function() {
-      this.health = 10;
-      this.name = "Enemy"
-    };
+    var enemyConstructor = function() {
+      var constructor = this;
+
+      var Enemy = function() {
+        this.maxHealth = 10;
+        this.health = 10;
+        this.name = "Enemy";
+        this.constructor = constructor;
+      };
+
+      constructor.build = function() {
+        return new Enemy();
+      }
+    }
 
     $scope.gameData = {
       bulletCount: 0,
@@ -15,14 +25,34 @@ angular.module("spaceRace", ['ngCookies'])
       villagesDestroyed: 0
     }
 
-    gc.currentTarget = new Enemy();
-
+    gc.currentTarget = (new enemyConstructor).build();
 
 
     gc.shootBullets = function(numberOfBullets) {
       $scope.gameData.bulletCount += numberOfBullets;
-      gc.currentTarget.health -= numberOfBullets;
-      handleAndCheckDeath();
+
+      if (numberOfBullets > gc.currentTarget.maxHealth) {
+        numberOfBullets -= gc.currentTarget.health; // Finish off first target
+
+        var targetsKilled = Math.floor(numberOfBullets/gc.currentTarget.maxHealth) + 1;
+        var bulletsRemaining = (numberOfBullets % gc.currentTarget.maxHealth);
+
+        $scope.gameData.resources += (10 * targetsKilled);
+        $scope.gameData.villagesDestroyed += (targetsKilled);
+
+        gc.currentTarget = gc.currentTarget.constructor.build();
+        gc.currentTarget.health -= bulletsRemaining;
+      } else if (numberOfBullets >= gc.currentTarget.health){
+        var bulletsRemaining = numberOfBullets - gc.currentTarget.health;
+
+        $scope.gameData.resources += 10;
+        $scope.gameData.villagesDestroyed += 1;
+
+        gc.currentTarget = gc.currentTarget.constructor.build();
+        gc.currentTarget.health -= bulletsRemaining;
+      } else {
+        gc.currentTarget.health -= numberOfBullets;
+      }
     }
 
     gc.recruitSoldier = function() {
@@ -36,17 +66,6 @@ angular.module("spaceRace", ['ngCookies'])
 
     gc.disableSoldierButton = function() {
       return ($scope.gameData.resources < 10);
-    }
-
-    function handleAndCheckDeath() {
-      if (gc.currentTarget.health <= 0) {
-        overkillAmount = gc.currentTarget.health * -1
-        gc.currentTarget = new Enemy();
-        gc.currentTarget.health -= overkillAmount;
-        $scope.gameData.resources += 10;
-        $scope.gameData.villagesDestroyed += 1;
-        handleAndCheckDeath(); // In case overkillAmount is >= new targetHealth
-      }
     }
 
     function autoSave() {
