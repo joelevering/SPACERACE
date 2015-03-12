fb77d7b374a62fcc85e724cee4264a5a023b2468
Joe Levering <coyoteejl@gmail.com>
2015-03-11 22:33:48 -0500

    Extract Enemy superclass and create factories for Enemy and EnemySoldier

--- init.js
+++ init.js
@@ -1,110 +1,115 @@
-angular.module("spaceRace", ['ngCookies'])
-  .controller("gameController", ['$scope', '$interval', '$cookieStore', function gameController($scope, $interval, $cookieStore){
-    var gc = this;
-    var versionNumber = 1
+var spaceRace = angular.module("spaceRace", ['ngCookies']);
 
-    var enemyConstructor = function() {
-      var constructor = this;
+spaceRace.factory("Enemy", function(){
+  var Enemy = function(attributes) {
+    this.health = attributes.health;
+    this.maxHealth = attributes.maxHealth;
+    this.name = attributes.name;
+    this.factory = attributes.factory;
+  };
 
-      var Enemy = function() {
-        this.maxHealth = 10;
-        this.health = 10;
-        this.name = "Enemy";
-        this.constructor = constructor;
-      };
+  return(Enemy);
+});
 
-      constructor.build = function() {
-        return new Enemy();
-      }
+var enemySoldierFactory = spaceRace.factory("EnemySoldier", ['Enemy', function(Enemy){
+  return {
+    build: function() {
+      return new Enemy({maxHealth: 10, health: 10, name: "Enemy Soldier", factory: this})
     }
+  }
+}]);
 
-    $scope.gameData = {
-      bulletCount: 0,
-      soldierCount: 0,
-      resources: 0,
-      villagesDestroyed: 0
+spaceRace.controller("gameController", ['$scope', '$interval', '$cookieStore', 'EnemySoldier', function gameController($scope, $interval, $cookieStore, EnemySoldier){
+  var gc = this;
+  var versionNumber = 1
+
+  $scope.gameData = {
+    bulletCount: 0,
+    soldierCount: 0,
+    resources: 0,
+    villagesDestroyed: 0
+  }
+
+  gc.currentTarget = EnemySoldier.build();
+
+
+  gc.shootBullets = function(numberOfBullets) {
+    $scope.gameData.bulletCount += numberOfBullets;
+
+    if (numberOfBullets > gc.currentTarget.maxHealth) {
+      numberOfBullets -= gc.currentTarget.health; // Finish off first target
+
+      var targetsKilled = Math.floor(numberOfBullets/gc.currentTarget.maxHealth) + 1;
+      var bulletsRemaining = (numberOfBullets % gc.currentTarget.maxHealth);
+
+      $scope.gameData.resources += (10 * targetsKilled);
+      $scope.gameData.villagesDestroyed += (targetsKilled);
+
+      gc.currentTarget = gc.currentTarget.factory.build();
+      gc.currentTarget.health -= bulletsRemaining;
+    } else if (numberOfBullets >= gc.currentTarget.health){
+      var bulletsRemaining = numberOfBullets - gc.currentTarget.health;
+
+      $scope.gameData.resources += 10;
+      $scope.gameData.villagesDestroyed += 1;
+
+      gc.currentTarget = gc.currentTarget.factory.build();
+      gc.currentTarget.health -= bulletsRemaining;
+    } else {
+      gc.currentTarget.health -= numberOfBullets;
     }
+  }
 
-    gc.currentTarget = (new enemyConstructor).build();
+  gc.recruitSoldier = function() {
+    $scope.gameData.soldierCount += 1;
+    $scope.gameData.resources -= 10;
+  }
 
+  gc.showSoldierButton = function() {
+    return ($scope.gameData.soldierCount > 0 || $scope.gameData.resources >= 10);
+  }
 
-    gc.shootBullets = function(numberOfBullets) {
-      $scope.gameData.bulletCount += numberOfBullets;
+  gc.disableSoldierButton = function() {
+    return ($scope.gameData.resources < 10);
+  }
 
-      if (numberOfBullets > gc.currentTarget.maxHealth) {
-        numberOfBullets -= gc.currentTarget.health; // Finish off first target
+  function autoSave() {
+    $cookieStore.put('version', versionNumber);
+    $cookieStore.put('gameData', $scope.gameData);
+    $cookieStore.put('timestampSeconds', secondsSinceEpoch());
+  }
 
-        var targetsKilled = Math.floor(numberOfBullets/gc.currentTarget.maxHealth) + 1;
-        var bulletsRemaining = (numberOfBullets % gc.currentTarget.maxHealth);
+  function secondsSinceEpoch() {
+    return new Date() / 1000;
+  }
 
-        $scope.gameData.resources += (10 * targetsKilled);
-        $scope.gameData.villagesDestroyed += (targetsKilled);
+  function secondsSinceLastLogin() {
+    var lastLoginTimestamp = $cookieStore.get('timestampSeconds');
 
-        gc.currentTarget = gc.currentTarget.constructor.build();
-        gc.currentTarget.health -= bulletsRemaining;
-      } else if (numberOfBullets >= gc.currentTarget.health){
-        var bulletsRemaining = numberOfBullets - gc.currentTarget.health;
-
-        $scope.gameData.resources += 10;
-        $scope.gameData.villagesDestroyed += 1;
-
-        gc.currentTarget = gc.currentTarget.constructor.build();
-        gc.currentTarget.health -= bulletsRemaining;
-      } else {
-        gc.currentTarget.health -= numberOfBullets;
-      }
+    if (lastLoginTimestamp) {
+      return Math.round( secondsSinceEpoch() - lastLoginTimestamp );
+    } else {
+      return 1;
     }
+  }
 
-    gc.recruitSoldier = function() {
-      $scope.gameData.soldierCount += 1;
-      $scope.gameData.resources -= 10;
+  function updateGameData(secondsPassed) {
+    gc.shootBullets($scope.gameData.soldierCount * secondsPassed);
+  }
+
+  var timer = $interval(function () {
+    updateGameData(1);
+
+    autoSave();
+  }, 1000, 0);
+
+  var init = function() {
+    if($cookieStore.get('version') === versionNumber) {
+      $scope.gameData = $cookieStore.get('gameData');
+
+      updateGameData(secondsSinceLastLogin());
     }
+  }
 
-    gc.showSoldierButton = function() {
-      return ($scope.gameData.soldierCount > 0 || $scope.gameData.resources >= 10);
-    }
-
-    gc.disableSoldierButton = function() {
-      return ($scope.gameData.resources < 10);
-    }
-
-    function autoSave() {
-      $cookieStore.put('version', versionNumber);
-      $cookieStore.put('gameData', $scope.gameData);
-      $cookieStore.put('timestampSeconds', secondsSinceEpoch());
-    }
-
-    function secondsSinceEpoch() {
-      return new Date() / 1000;
-    }
-
-    function secondsSinceLastLogin() {
-      var lastLoginTimestamp = $cookieStore.get('timestampSeconds');
-
-      if (lastLoginTimestamp) {
-        return Math.round( secondsSinceEpoch() - lastLoginTimestamp );
-      } else {
-        return 1;
-      }
-    }
-
-    function updateGameData(secondsPassed) {
-      gc.shootBullets($scope.gameData.soldierCount * secondsPassed);
-    }
-
-    var timer = $interval(function () {
-      updateGameData(1);
-
-      autoSave();
-    }, 1000, 0);
-
-    var init = function() {
-      if($cookieStore.get('version') === versionNumber) {
-        $scope.gameData = $cookieStore.get('gameData');
-
-        updateGameData(secondsSinceLastLogin());
-      }
-    }
-
-    init();
+  init();
 }]);
